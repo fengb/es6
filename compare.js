7bcd526c4880e07e79db9a535e96240b04ca57b1
fengb <contact@fengb.info>
2015-06-04 13:21:45 -0500

    Compare with ES6

--- compare.js
+++ compare.js
@@ -1,72 +1,127 @@
-###
+/*
 -- Fat arrow
-###
+ */
 
-[0, 1, 2].map (val) => (val * val)
+[0, 1, 2].map((val) => (val * val))
 
-###
+/*
 -- Class
-###
+ */
 
-class Rectangle
-  constructor: (width, height) ->
-    @width = width
-    @height = height
+class Rectangle {
+  constructor(width, height){
+    this.width = width
+    this.height = height
+  }
 
-  area: ->
-    @width * @height
+  area(){
+    return this.width * this.height
+  }
 
-  @area: (width, height) ->
-    new Rectangle(width, height).area()
+  static area(){
+    return new Rectangle(width, height).area()
+  }
+}
 
-class Square extends Rectangle
-  constructor: (side) ->
+class Square extends Rectangle {
+  constructor(side){
     super(side, side)
+  }
+}
 
-###
+/*
 -- Object literals
-###
+ */
 
-shorthand = 'foo'
-obj =
-  shorthand # short for shorthand: shorthand
-  method: -> undefined
-obj['expr' + 'ession'] = true
+var shorthand = 'foo'
+var obj = {
+  shorthand, // short for shorthand: shorthand
+  method(){
+  },
+  ['expr' + 'ession']: true
+}
 
-###
+/*
 -- Template strings
-###
+ */
 
-"Hello #{name}"
+`Hello ${name}`
 
-###
+/*
 -- Destructured assignment
-###
+ */
 
-a, b, c = [1, 2, 3]
+var [a, b, c] = [1, 2, 3]
 
-###
+/*
 -- Default arguments
-###
+ */
 
-f = (a, b, c = 1) ->
-  a + b + c
+function f(a, b, c = 1){
+  return a + b + c
+}
 
-###
+/*
 -- Splats
-###
+ */
 
-s = (a, args...) ->
-  a * args.length
+function s(a, ...args){
+  return a * args.length
+}
 
-s(1, [2, 3, 4, 5]...)
+s(1, ...[2, 3, 4, 5])
 
-###
+/*
 -- Iteration
-###
+ */
 
-for n in [1, 2, 3]
-  console.log i
+for(var n of [1, 2, 3]){
+  console.log(n)
+}
 
-for n, i in [1, 4, 9]
-  console.log i, n
+for(var [i, n] of [1, 4, 9].entries()){
+  console.log(i, n)
+}
+
+/*
+-- let / const
+ */
+
+let funcs = []
+for(var i of [1, 2, 3]){
+  funcs.push(() => i) // binds i to function value (global)
+}
+
+for(let i of [1, 2, 3]){
+  funcs.push(() => i) // binds i to block value
+}
+
+const K = 1
+K = 2 // error
+
+/*
+-- generators
+ */
+
+function* range(n){
+  for(let i = 0; i < n; i++){
+    yield i
+  }
+}
+
+for(let i of range(5)){
+  console.log(i)
+}
+
+/*
+-- modules
+ */
+
+import Ember from 'ember'
+import { Route, Array as EmberArray } from 'ember'
+import * as emberPackage from 'ember'
+
+export class Awesome {
+}
+
+export default Awesome
